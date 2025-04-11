49804a6120165c77d1863d8f2d5338565ac6c00d
mrfour4 <21521609@gm.uit.edu.vn>
2025-04-11 11:19:51 +0700

    test: build fail

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter';
+// import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
